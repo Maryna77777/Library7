015a6601a49d465833d188b1e886ce38baa1927b
Maryna <nikishina.maryna@gmail.com>
2022-01-18 17:00:12 +0200

    fix

--- target/generated-sources/annotations/com/example/mapper/ActorMapperImpl.java
+++ target/generated-sources/annotations/com/example/mapper/ActorMapperImpl.java
@@ -1,12 +1,16 @@
 package com.example.mapper;
 
 import com.example.DTO.ActorDTO;
+import com.example.DTO.FilmDTO;
 import com.example.entity.Actor;
+import com.example.entity.Film;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-01-18T14:11:36+0200",
+    date = "2022-01-18T16:58:19+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.13 (Ubuntu)"
 )
 public class ActorMapperImpl implements ActorMapper {
@@ -22,7 +26,52 @@ public class ActorMapperImpl implements ActorMapper {
         actorDTO.setId( actor.getId() );
         actorDTO.setFirstName( actor.getFirstName() );
         actorDTO.setLastName( actor.getLastName() );
+        actorDTO.setFilms( filmListToFilmDTOList( actor.getFilms() ) );
 
         return actorDTO;
+    }
+
+    protected List<ActorDTO> actorListToActorDTOList(List<Actor> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<ActorDTO> list1 = new ArrayList<ActorDTO>( list.size() );
+        for ( Actor actor : list ) {
+            list1.add( fromActor( actor ) );
+        }
+
+        return list1;
+    }
+
+    protected FilmDTO filmToFilmDTO(Film film) {
+        if ( film == null ) {
+            return null;
+        }
+
+        FilmDTO filmDTO = new FilmDTO();
+
+        if ( film.getId() != null ) {
+            filmDTO.setId( film.getId() );
+        }
+        filmDTO.setTitle( film.getTitle() );
+        filmDTO.setYear( film.getYear() );
+        filmDTO.setCountry( film.getCountry() );
+        filmDTO.setActors( actorListToActorDTOList( film.getActors() ) );
+
+        return filmDTO;
+    }
+
+    protected List<FilmDTO> filmListToFilmDTOList(List<Film> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<FilmDTO> list1 = new ArrayList<FilmDTO>( list.size() );
+        for ( Film film : list ) {
+            list1.add( filmToFilmDTO( film ) );
+        }
+
+        return list1;
     }
 }
